cac7c93c9e05be73dffcf4dbce48c662cb5ef743
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-07-03 15:07:09 +0200

    Completed tabs

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,49 +3,13 @@ import { useParams } from "react-router-dom";
 import { getItem } from '../services/itemService';
 import FullScreenLoadingIndicator from './FullScreenLoadingIndicator';
 import Carousel from 'react-material-ui-carousel';
-import DetailItem, {caracteristic} from '../interfaces/DetailItem';
+import DetailItem, { caracteristic } from '../interfaces/DetailItem';
+import TabPanel from './TabPanel';
 import '../styles/Item.css';
 
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-
 
 
 export default function Item() {
@@ -89,13 +53,16 @@ export default function Item() {
         <Box sx={{ width: '100%' }}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
             <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-              <Tab label="Description" {...a11yProps(0)} />
-              <Tab label="Caractéristiques" {...a11yProps(1)} />
+              <Tab label="Description" />
+              <Tab label="Caractéristiques" />
             </Tabs>
           </Box>
+
+
           <TabPanel value={value} index={0}>
             {item?.description}
           </TabPanel>
+
           <TabPanel value={value} index={1}>
             {item && item?.caracteristics.map((el: caracteristic) => {
               return (
---
+++ src/components/TabPanel.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+export default function TabPanel(props: TabPanelProps) {
+  const { children, value, index } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
